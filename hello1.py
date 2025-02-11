ccae256a61570f02afa527bafd7f23d8ab7824f0
Adish94 <adishpandit10@gmail.com>
2025-02-11 09:41:33 +0530

    Update hello1.py

--- hello1.py
+++ hello1.py
@@ -1,4 +1,10 @@
 a=int(input("enter 1st no"))
 b=int(input("enter 2nd no"))
-ans=a+b
-print("aditionn of number is ",ans)
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+print("aditionn of number is ",add)
+print("subtraction of number is ",sub)
+print("multiplication of number is ",mul)
+print("division of number is ",div)
